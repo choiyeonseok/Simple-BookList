683096db93a82f499af337ac2ee92c128a917396
choiyeonseok <smr603@snu.ac.kr>
2020-01-02 00:39:55 +0900

    Create and Connect async main function with HTML

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hello yeonseok');
+async function main() { 
+
+    // 버튼에 이벤트 연결
+
+    // 토큰 체크
+
+    // 토큰으로 서버에서 나의 정보 받아오기
+
+    //나의 책을 서버에서 받아오기
+
+    //받아온 책을 그리기
+
+}
+
+document.addEventListener('DOMContentLoaded', main);  // 함수호출없이 자동으로 실행
+/**
+ * DOMContentLoaded 이벤트는 최초 HTML문서가 완전히 로드 및 파싱되었을때 발생되고, 
+ * 스타일시트나 이미지 및 서브프레임 로드가 끝나기를 기다리지 않는다.
+ * load 이벤트는 오직 모든 페이지가 완전히 로드되었을 때 사용해야 한다. 
+ * 즉 DOMContentLoaded가 load보다 더 빨리 일어난다.
+ */
