8a811cc5cb887d42d87d91659cf3f143bba30613
choiyeonseok <smr603@snu.ac.kr>
2020-01-02 01:04:55 +0900

    Add getting books from server by token

--- index.js
+++ index.js
@@ -21,6 +21,20 @@ async function getUserByToken(token) {
     }
 } 
 
+async function getBooks(token) {
+    try {
+        const res = axios.get('https://api.marktube.tv/v1/book', {
+            headers: {
+                Authorization: `Bearer ${token}`;
+            },
+        });
+        return res.data;
+    } catch (error) {
+        consonle.log('getBooks error', error);
+        return null;
+    }
+}
+
 async function main() { 
 
     // 버튼에 이벤트 연결
@@ -36,12 +50,16 @@ async function main() {
     // 토큰으로 서버에서 나의 정보 받아오기
     const user = await getUserByToken(token);
     if (user === null) {
-        localStorage.clear();
+        localStorage.clear();  //token을 없앰으로써 logout을 시킨다.
         location.assign('/login');
         return;
     }
 
     //나의 책을 서버에서 받아오기
+    const books = await getBooks(token);
+    if (books === null) {
+        return;
+    }
 
     //받아온 책을 그리기
 
