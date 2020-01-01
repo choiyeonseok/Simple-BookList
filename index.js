2974ba12674bb75fcad058ff049918e63506b60e
choiyeonseok <smr603@snu.ac.kr>
2020-01-02 00:55:07 +0900

    Add binding LogoutButton with main logic

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+function bindLogoutButton() {
+    const btnLogout = document.querySelector('#btn_logout');
+    btnLogout.addEventListener('click', logout);
+}
+
 async function main() { 
 
     // 버튼에 이벤트 연결
-
+    bindLogoutButton();
+    
     // 토큰 체크
 
     // 토큰으로 서버에서 나의 정보 받아오기
